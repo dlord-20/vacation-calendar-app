49c73dcbb8a3adb1676e151df9ea91990d842a46
dlord-20 <dthomaslord@gmail.com>
2022-07-24 15:36:41 +0300

    Working functions for left time panel on calendar. Code it now easier to read and easier to change -> just need to update the css modular now

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -48,12 +48,43 @@ export default function Calendar() {
         return times;
     }
 
-    const getTimeOfDayDiv = () => {
-        <div className={`${calendarStyles.timeOfDay} ${calendarStyles.sixAm}`}>6:00 am</div>
+    const getTimeOfDayDiv = (hour, halfHour, am, row) => {
+        let half = 0;
+        if(halfHour) {
+            half = 3;
+        }
+        let amPm = 'a'
+        if(!am) {
+            amPm = 'p'
+        }
+        return <div className={calendarStyles.timeOfDay} style={{gridArea: `${row}/1/${row + 1}/2`}}>{hour}:{half}0 {amPm}m</div>;
     }
 
     const getTimeOfDayDivs = () => {
-        const times = []
+        const times = [];
+        let row = 2;
+        //get am times
+        for(let i = 6; i < 12; i++) {
+            times.push(getTimeOfDayDiv(i, false, true, row));
+            row++;
+            times.push(getTimeOfDayDiv(i, true, true, row));
+            row++;
+        }
+
+        //get noon times
+        times.push(getTimeOfDayDiv(12, false, false, row));
+        row++;
+        times.push(getTimeOfDayDiv(12, false, false, row));
+        row++;
+
+        //get pm times
+        for(let i = 1; i < 9; i++) {
+            times.push(getTimeOfDayDiv(i, false, false, row));
+            row++;
+            times.push(getTimeOfDayDiv(i, true, false, row));
+            row++;
+        }
+        return times;
     }
 
     return(
@@ -90,36 +121,6 @@ export default function Calendar() {
                 <div className={`${calendarStyles.weekdayHeaders} ${calendarStyles.saturday}`}><p>Saturday</p></div>
                 {/* Time of Day */}
                 {getTimeOfDayDivs()}
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.sixAm}`}>6:00 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.sixThirtyAm}`}>6:30 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.sevenAm}`}>7:00 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.sevenThirtyAm}`}>7:30 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.eightAm}`}>8:00 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.eightThirtyAm}`}>8:30 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.nineAm}`}>9:00 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.nineThirtyAm}`}>9:30 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.tenAm}`}>10:00 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.tenThirtyAm}`}>10:30 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.elevenAm}`}>11:00 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.elevenThirtyAm}`}>11:30 am</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.twelvePm}`}>12:00 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.twelveThirtyPm}`}>12:30 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.onePm}`}>1:00 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.oneThirtyPm}`}>1:30 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.twoPm}`}>2:00 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.twoThirtyPm}`}>2:30 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.threePm}`}>3:00 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.threeThirtyPm}`}>3:30 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.fourPm}`}>4:00 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.fourThirtyPm}`}>4:30 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.fivePm}`}>5:00 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.fiveThirtyPm}`}>5:30 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.sixPm}`}>6:00 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.sixThirtyPm}`}>6:30 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.sevenPm}`}>7:00 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.sevenThirtyPm}`}>7:30 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.eightPm}`}>8:00 pm</div>
-                <div className={`${calendarStyles.timeOfDay} ${calendarStyles.eightThirtyPm}`}>8:30 pm</div>
 
                 <div style={{...getBorderStyle(), ...getGridPlacement(6,6,7,7)}}>I'm a test</div>
                 <div style={{gridArea: 2/4/3/5}}>I'm the second</div>
