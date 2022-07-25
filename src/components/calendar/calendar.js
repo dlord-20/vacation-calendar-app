2b18e10eec0c367133212fba9b3e3f493d6eebb6
dlord-20 <dthomaslord@gmail.com>
2022-07-25 21:33:15 +0300

    Working calendar with correct placement. Need to refactor the timeOfDay div functions to adjust to the new timesTest array

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -6,11 +6,12 @@ export default function Calendar() {
     const [userEvents, setUserEvents] = useState([]);
     const [eventName, setEventName] = useState('');
     const [eventDayOfWeek, setEventDayOfWeek] = useState('Sunday');
-    const [eventTimeOfDay, setEventTimeOfDay ] = useState('6: 00 am');
+    const [eventTimeOfDay, setEventTimeOfDay ] = useState('6:00 am');
     const [eventDuration, setEventDuration ]  = useState(0.5);
 
     //add data arrays for time and weekday
     const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const timesTest = ['6:00 am', '6:30 am', '7:00 am', '7:30 am', '8:00 am', '8:30 am', '9:00 am', '9:30 am', '10:00 am', '10:30 am', '11:00 am', '11:30 am', '12:00 pm', '12:30 pm', '1:00 pm', '1:30 pm', '2:00 pm', '2:30 pm', '3:00 pm', '3:30 pm', '4:00 pm', '4:30 pm', '5:00 pm', '5:30 pm', '6:00 pm', '6:30 pm', '7:00 pm', '7:30 pm', '8:00 pm', '8:30 pm', ];
 
     const getBorderStyle = () => {
         return {
@@ -130,18 +131,18 @@ export default function Calendar() {
     }
 
     const getRowStartPosition = (timeOfDay) => {
-        // console.log(timeOfDay);
-        return 5;
+        const index = timesTest.indexOf(timeOfDay);
+        return index + 2;
     }
 
     const getRowEndPosition = (eventDuration) => {
-        return 1;
+        return eventDuration * 2;
     }
 
     const getColumnStartPosition = (weekday) => {
         // console.log(weekday);
-
-        return 5;
+        const index = weekdays.indexOf(weekday);
+        return index + 2;
     }
     
     const getEventDiv = (event) => {
@@ -151,7 +152,6 @@ export default function Calendar() {
         const colStart = getColumnStartPosition(event.eventWeekday);
 
         return <div style={{...getBorderStyle(), ...getGridPlacement(rowStart, colStart, rowStart + rowEnd, colStart + 1)}}>{event.eventName}</div>
-        // <div style={{...getBorderStyle(), ...getGridPlacement(6,6,7,7)}}>I'm a test</div>
     }
     
     const getUserEvents = () => {
