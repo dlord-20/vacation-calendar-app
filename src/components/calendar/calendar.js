6f9ff82260f382dea34d34e0706c93547183f544
dlord-20 <dthomaslord@gmail.com>
2022-07-26 16:09:07 +0300

    Category color starting to get implemented. Need a selector for the form and complete the getBorderStyle method

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -17,6 +17,8 @@ export default function Calendar() {
     const weekdaysData = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const timesData = ['6:00 am', '6:30 am', '7:00 am', '7:30 am', '8:00 am', '8:30 am', '9:00 am', '9:30 am', '10:00 am', '10:30 am', '11:00 am', '11:30 am', '12:00 pm', '12:30 pm', '1:00 pm', '1:30 pm', '2:00 pm', '2:30 pm', '3:00 pm', '3:30 pm', '4:00 pm', '4:30 pm', '5:00 pm', '5:30 pm', '6:00 pm', '6:30 pm', '7:00 pm', '7:30 pm', '8:00 pm', '8:30 pm' ];
 
+    const categoryColorsData = [{ colorName: 'blue', colorCode: '555555'},{ colorName: 'green', colorCode: '123456'}]
+
     //----------------End of Data---------------------
 
     //----------------Determine CSS---------------------
@@ -24,7 +26,9 @@ export default function Calendar() {
     //Determine className for the event
     const getBorderStyle = () => {
         return {
-            border: '1px green solid'
+            border: '1px green solid',
+            backgroundColor: `#${categoryColorsData[1].colorCode}`,
+            color: 'white'
         }
     }
 
