22783ec94bf9c00f75d32e591dd08bb15f330607
dlord-20 <dthomaslord@gmail.com>
2022-07-26 09:38:46 +0300

    Added documentation for current state of the application

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -10,50 +10,58 @@ export default function Calendar() {
     const [eventDuration, setEventDuration ]  = useState(0.5);
 
     //add data arrays for time and weekday
-    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const timesTest = ['6:00 am', '6:30 am', '7:00 am', '7:30 am', '8:00 am', '8:30 am', '9:00 am', '9:30 am', '10:00 am', '10:30 am', '11:00 am', '11:30 am', '12:00 pm', '12:30 pm', '1:00 pm', '1:30 pm', '2:00 pm', '2:30 pm', '3:00 pm', '3:30 pm', '4:00 pm', '4:30 pm', '5:00 pm', '5:30 pm', '6:00 pm', '6:30 pm', '7:00 pm', '7:30 pm', '8:00 pm', '8:30 pm' ];
+    const weekdaysData = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const timesData = ['6:00 am', '6:30 am', '7:00 am', '7:30 am', '8:00 am', '8:30 am', '9:00 am', '9:30 am', '10:00 am', '10:30 am', '11:00 am', '11:30 am', '12:00 pm', '12:30 pm', '1:00 pm', '1:30 pm', '2:00 pm', '2:30 pm', '3:00 pm', '3:30 pm', '4:00 pm', '4:30 pm', '5:00 pm', '5:30 pm', '6:00 pm', '6:30 pm', '7:00 pm', '7:30 pm', '8:00 pm', '8:30 pm' ];
 
+    //Determine className for the event
     const getBorderStyle = () => {
         return {
             border: '1px green solid'
         }
     }
 
+    //Place event in correct position on the calendar
     const getGridPlacement = (rowStart, colStart, rowEnd, colEnd) => {
         return {
             gridArea: `${rowStart}/${colStart}/${rowEnd}/${colEnd}`
         }
     }
 
+    //Return an option for the 'Time of the Day' dropdown list in the form
     const getOptions = (time) => {
         return (
-        <option value={time}>
-            <p>{time}</p>
-        </option>)
+            <option value={time}>
+                <p>{time}</p>
+            </option>
+        )
     }
 
+    //Return list of options for the 'Time of the Day' in the form
     const getTimeOfDayOptions = () => {
         const times = [];
 
-        timesTest.forEach(time => {
+        timesData.forEach(time => {
             times.push(getOptions(time));
         })
 
         return times;
     }
 
+    //Returns an time div for the calendar (left panel)
     const getTimeOfDayDiv = (time, row) => {
         return (
-        <div className={calendarStyles.timeOfDay} style={{gridArea: `${row}/1/${row + 1}/2`}}>
-            <p>{time}</p>
-        </div>);
+            <div className={calendarStyles.timeOfDay} style={{gridArea: `${row}/1/${row + 1}/2`}}>
+                <p>{time}</p>
+            </div>
+        );
     }
 
+    //Adds a list of time divs that is added to the DOM (left panel)
     const getTimeOfDayDivs = () => {
         const times = [];
         let row = 2;
 
-        timesTest.forEach(time => {
+        timesData.forEach(time => {
             times.push(getTimeOfDayDiv(time, row));
             row++
         })
@@ -61,11 +69,12 @@ export default function Calendar() {
         return times;
     }
 
+    //returns an array of weekday headers that is added to the DOM
     const getWeekdayHeadersDivs = () => {
         const weekdaysArray = [];
         let col = 2;
-        for(let i = 0; i < weekdays.length; i++) {
-            const newDay = <div className={calendarStyles.weekdayHeaders} style={{gridArea: `1/${col}/2/${col + 1}`}}>{weekdays[i]}</div>;
+        for(let i = 0; i < weekdaysData.length; i++) {
+            const newDay = <div className={calendarStyles.weekdayHeaders} style={{gridArea: `1/${col}/2/${col + 1}`}}>{weekdaysData[i]}</div>;
             weekdaysArray.push(newDay);
             col++;
         }
@@ -73,15 +82,17 @@ export default function Calendar() {
         return weekdaysArray;
     }
 
+    //returns an array of options for weekday headers that is added to the form
     const getWeekdayHeadersOptions = () => {
         const weekdayOptions = [];
-        for(let i = 0; i < weekdays.length; i++) {
-            const option = <option value={weekdays[i]}>{weekdays[i]}</option>
+        for(let i = 0; i < weekdaysData.length; i++) {
+            const option = <option value={weekdaysData[i]}>{weekdaysData[i]}</option>
             weekdayOptions.push(option)
         }
         return weekdayOptions;
     }
 
+    //returns an array of options in the 'Duration" question in the form
     const getDurationOptions = () => {
         const durations = [];
         for(let i = 1; i < 11; i++) {
@@ -94,21 +105,25 @@ export default function Calendar() {
         return list;
     }
 
+    //returns row position for the event in the calendar
     const getRowStartPosition = (timeOfDay) => {
-        const index = timesTest.indexOf(timeOfDay);
+        const index = timesData.indexOf(timeOfDay);
         return index + 2;
     }
 
+    //returns how long the event will last in the calendar
     const getRowEndPosition = (eventDuration) => {
         return eventDuration * 2;
     }
 
+    //returns what day to put the event
     const getColumnStartPosition = (weekday) => {
         // console.log(weekday);
-        const index = weekdays.indexOf(weekday);
+        const index = weekdaysData.indexOf(weekday);
         return index + 2;
     }
     
+    //returns an event div for the calendar
     const getEventDiv = (event) => {
         // console.log(event.newName);
         const rowStart = getRowStartPosition(event.eventTime);
@@ -118,6 +133,7 @@ export default function Calendar() {
         return <div style={{...getBorderStyle(), ...getGridPlacement(rowStart, colStart, rowStart + rowEnd, colStart + 1)}}>{event.eventName}</div>
     }
     
+    //returns an array of events that is added to the DOM
     const getUserEvents = () => {
         const events = [];
         
@@ -130,17 +146,19 @@ export default function Calendar() {
         return events;
     }
 
+    //creates div for list below the calendar
     const getEventListDiv = (event) => {
         return (
         <div className={calendarStyles.eventList}>
             <p>{event.eventName}</p>
             <p>{event.eventWeekday}</p>
             <p>{event.eventTime}</p>
-            <p>{event.eventDuration}</p>
+            <p>{event.eventDuration} hours</p>
         </div>
         )
     }
 
+    //adds event list to the DOM
     const getUserEventsList = () => {
         const events = [];
 
@@ -152,6 +170,7 @@ export default function Calendar() {
         return events;
     }
 
+    //handles form submission
     const handleSubmit = (event) => {
         event.preventDefault();
         const newEvent = {
@@ -161,7 +180,11 @@ export default function Calendar() {
             eventDuration: eventDuration
         } 
         setUserEvents(userEvents => [...userEvents, newEvent])
+
+        //Figure out how to reset the form after a submission
     }
+
+    //Updates states in the app
 
     const handleNameChange = (e) => {
         setEventName(eventName =>  e.target.value);
